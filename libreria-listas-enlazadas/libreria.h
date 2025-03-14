f7e48d0c9f8df89c79f8164a3d9a8a0d52f3f339
david <david20vn@gmail.com>
2025-03-13 19:53:15 -0500

    finalizacion libreria de funciones para listas simplemente enlazadas

--- libreria-listas-enlazadas/libreria.h
+++ libreria-listas-enlazadas/libreria.h
@@ -12,11 +12,32 @@ typedef struct Nodo {
     struct Nodo *sig;
 } Nodo;
 
+int menu(){
+	
+	int opcion;
+	
+	printf("\n\t\tMENU\n\n");
+	printf("\t1) agregar al comienzo\n");
+	printf("\t2) agregar al final\n");
+	printf("\t3) agregar en posicion\n");
+	printf("\t4) agregar ordenado\n");
+	printf("\t5) eliminar\n");
+	printf("\t6) modificar\n");
+	printf("\t7) consultar\n");
+	printf("\t8) mostrar lista\n");
+	printf("\nOpcion: ");
+	scanf ("%d", &opcion);
+	printf("\n");
+	
+	return opcion;
+	
+}
+
 void muestra(Nodo *lista) {
     Nodo *aux;
     printf("->");
     for (aux = lista; aux != NULL; aux = aux->sig) {
-        printf("[%s]-> ", aux->info.nombre);
+        printf("[%s - %d]-> ", aux->info.nombre, aux->info.id);
     }
     printf("\n");
 }
@@ -30,15 +51,15 @@ void lee_nodo(Info *inf) {
     scanf(" %[^\n]", w);
     printf("\n");
     inf->id = i;
-    pw = malloc(strlen(w) + 1);
+    pw = (char*)malloc(strlen(w) + 1);
     strcpy(pw, w);
     inf->nombre = pw;
 }
 
 Nodo* insertar_comienzo(Nodo *lista) {
-    Info *inf = malloc(sizeof(Info));
+    Info *inf = (Info*)malloc(sizeof(Info));
     lee_nodo(inf);
-    Nodo *nuevo = malloc(sizeof(Nodo));
+    Nodo *nuevo = (Nodo*)malloc(sizeof(Nodo));
     nuevo->info = *inf;
     nuevo->sig = lista;
     lista = nuevo;
@@ -46,26 +67,31 @@ Nodo* insertar_comienzo(Nodo *lista) {
 }
 
 Nodo* insertar_final(Nodo *lista) {
-    Info *inf = malloc(sizeof(Info));
+    Info *inf = (Info*)malloc(sizeof(Info));
     lee_nodo(inf);
-    Nodo *nuevo = malloc(sizeof(Nodo));
+    Nodo *nuevo = (Nodo*)malloc(sizeof(Nodo));
     nuevo->info = *inf;
-    Nodo *actual;
-   
-    for ( actual = lista; actual != NULL; actual = actual->sig){
-    if ( actual->sig == NULL ){
-    actual->sig = nuevo;
+    Nodo *actual = lista;
+    
     nuevo->sig = NULL;
-}
-}
-
+    
+    if ( lista == NULL ){
+    	return nuevo;
+		}
+   
+    while ( actual->sig != NULL ){
+    	actual = actual->sig;
+		}
+	
+		actual->sig = nuevo;
     return lista;
 }
 
 Nodo* insertar_posicion(Nodo *lista, int posicion) {
-    Info *inf = malloc(sizeof(Info));
+
+    Info *inf = (Info*)malloc(sizeof(Info));
     lee_nodo(inf);
-    Nodo *nuevo = malloc(sizeof(Nodo));
+    Nodo *nuevo = (Nodo*)malloc(sizeof(Nodo));
     nuevo->info = *inf;
    
     if (posicion == 0) {
@@ -88,9 +114,9 @@ Nodo* insertar_posicion(Nodo *lista, int posicion) {
 
 Nodo* insertar_ordenado(Nodo *lista) {
 	
-    Info *inf = malloc(sizeof(Info));
+    Info *inf = (Info*)malloc(sizeof(Info));
     lee_nodo(inf);
-    Nodo *nuevo = malloc(sizeof(Nodo));
+    Nodo *nuevo = (Nodo*)malloc(sizeof(Nodo));
     nuevo->info = *inf;
     Nodo *aux = lista;
    
@@ -100,7 +126,7 @@ Nodo* insertar_ordenado(Nodo *lista) {
     }
     
    
-    while ( nuevo->info.id > aux->info.id) {
+    while ( aux->sig != NULL && nuevo->info.id > aux->sig->info.id) {
     	aux = aux->sig;
     }
 
@@ -111,3 +137,90 @@ Nodo* insertar_ordenado(Nodo *lista) {
     return lista;
     
 }
+
+int buscar(Nodo *lista, int id_buscar){
+	
+	int i = 0;
+	Nodo *aux = lista;
+	
+	while ( aux->info.id != id_buscar && aux != NULL ){
+		i++;
+		aux = aux->sig;
+	}
+	
+	if ( aux == NULL ){
+		return -1;
+	} else {
+		return i;
+	}	
+}
+
+Nodo* eliminar(Nodo *lista, int id_eliminar) {
+	
+    int posicion = buscar(lista, id_eliminar), i;
+    
+    if (posicion == -1) {
+        printf("\nEl id no está en la lista.\n");
+        return lista;
+    }
+
+    Nodo *aux = lista;
+    
+    if (posicion == 0) {
+        Nodo *temp = lista;
+        lista = lista->sig;
+        free(temp);
+        return lista;
+    }
+
+    for ( i = 0; i < posicion-1; i++) {
+        aux = aux->sig;
+    }
+
+    Nodo *temp = aux->sig;
+    aux->sig = aux->sig->sig;
+    free(temp);
+
+    return lista;
+}
+
+
+Nodo* modificar ( Nodo* lista, int id_modificar ){
+	
+	int posicion = buscar (lista, id_modificar);
+	
+	if ( posicion == -1 ){
+		printf ("\nEl id no esta en la lista.");
+		return lista;
+	}
+	
+	lista = eliminar(lista, id_modificar);
+	lista = insertar_posicion ( lista, posicion);
+	
+	return lista;
+}
+
+void consultar ( Nodo* lista, int id_consultar ){
+	
+	int i, posicion = buscar ( lista, id_consultar );
+	
+	if (posicion == -1) {
+        printf("\nEl id no está en la lista.\n");
+        return;
+    }
+    
+	Nodo *aux = lista;
+	
+	for ( i = 0; i < posicion; i++ ){
+		aux = aux->sig;
+	}
+	
+	printf ( "\nId: %d\n", aux->info.id );
+	printf ( "nombre: %s\n", aux->info.nombre );
+}
+
+
+
+
+
+
--- libreria-listas-enlazadas/main.c
+++ libreria-listas-enlazadas/main.c
@@ -3,16 +3,48 @@
 #include "libreria.h"
 
 int main() {
+	
     Nodo *lista = NULL;  // Inicializar la lista como vacía
-
-    // Insertar nodos
-    lista = insertar_comienzo(lista);
-    lista = insertar_final(lista);
-		lista = insertar_posicion(lista, 2);
-		lista = insertar_ordenado(lista);
-
-		//mostrar la lista
-    muestra(lista);
+    int opcion, posicion, id;
+    do {
+    	opcion = menu();
+    
+			switch ( opcion ){
+				case 1:
+					lista = insertar_comienzo(lista);
+					break;
+				case 2:
+					lista = insertar_final(lista);
+					break;
+				case 3:
+					printf ("Digite la posicion para insertar: ");
+					scanf ("%d", &posicion);
+					lista = insertar_posicion(lista, posicion);
+					break;
+				case 4:
+					printf ("'el nodo se pondra en la posicion inmediatamente anterior a la de el primer id con valor mayor a el del nodo ingresado'\n\n");
+					lista = insertar_ordenado(lista);
+					break;
+				case 5:
+					printf ("Digite el id del nodo a eliminar: ");
+					scanf ("%d", &id);
+					lista = eliminar( lista, id);
+					break;
+				case 6: 
+					printf ("Digite el id del nodo a modificar: ");
+					scanf ("%d", &id);
+					lista = modificar(lista, id);
+					break;
+				case 7:
+					printf ("Digite el id del nodo a consultar: ");
+					scanf ("%d", &id);
+					consultar(lista, id);
+					break;
+				case 8: 
+					muestra(lista);
+					break;
+			}
+		} while ( opcion != 9 );
 
     return 0;
 }
